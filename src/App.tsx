27a88f3689164598ddfbaa421c3110b65ab8ea7a
Md. Osman Faruk <programmming.code@gmail.com>
2021-08-23 20:13:14 +0600

    project archite created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import AppRouter from './AppRouter';
+import Header from './components/common/Header';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AppRouter>
+        <Header />
+      </AppRouter>
+    </div>
+  );
 };
 
 export default App;
